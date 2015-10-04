991a4eeecb19fdb9c8856ddf0fdcca35563f3942
DAVID HERNANDEZ <davfep_96@hotmail.com>
2015-10-04 16:56:57 -0500

    Reorganizando, comenzando el Manejador.

---
+++ src/MyP2/Manejador.java
@@ -0,0 +1,19 @@
+package MyP2;
+
+import java.sql.*;
+import java.util.*;
+
+public class Manejador{
+
+	public LinkedList<String> buscar(){
+
+	}
+
+	public void elimina(){
+
+	}
+
+	public void agrega(){
+		
+	}
+}
---
+++ src/MyP2/VistaController.java
@@ -0,0 +1,32 @@
+package MyP2;
+
+import java.sql.*;
+import java.util.*;
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.TextField;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextArea;
+
+
+/**
+ *
+ * @author davif
+ * Clase Controladora
+ */
+public class VistaController implements Initializable {
+    
+
+    
+    /**
+    * Metodo que inicializa objetos desde que abre interfaz.
+    */
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+
+    }    
+    
+}
